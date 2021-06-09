8a81e40ae85ec05cc39249b84e093ad6d85bfe52
jorgt <jorg.thuijls@gmail.com>
2021-06-09 12:01:54 +1000

    adding npx to start cmd

--- auth/passport.js
+++ auth/passport.js
@@ -1,7 +1,8 @@
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
-const { GITHUB_CALLBACK_URL, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, VERCEL_URL } = process.env;
+const { GITHUB_CALLBACK_URL, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
 
+console.log(GITHUB_CALLBACK_URL)
 passport.serializeUser(function (user, done) {
   done(null, user);
 });
@@ -13,7 +14,7 @@ passport.deserializeUser(function (user, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: `${VERCEL_URL}${GITHUB_CALLBACK_URL}`
+  callbackURL: GITHUB_CALLBACK_URL
 },
   function (accessToken, refreshToken, profile, done) {
     return done(null, profile);
